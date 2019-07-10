54fb80acdfe398c1a671302255b7767ac9190b71
Nick Foden <nickfoden@gmail.com>
2019-07-09 23:20:40 -0400

    Comment out unused styles import

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Container, Input, Button } from "pcln-design-system";
 import { signIn } from "../actions";
-import styles from "./styles.module.css";
+// import styles from "./styles.module.css";
 
 const INITIAL_STATE = {
   email: "",
